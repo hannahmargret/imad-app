db00c0790a654ed532a1b2b314ea6b9932a67c28
hannahmargret <30620128+hannahmargret@users.noreply.github.com>
2017-08-17 14:07:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight()
+{
+   marginLeft = marginLeft+5;
+   img.style.marginLeft = marginLeft + "px";
+}
 img.onclick = function(){
-  img.style.marginLeft = "100px";  
+    var interval = setInterval(moveRight,100);
+  //img.style.marginLeft = "100px";  
 };
