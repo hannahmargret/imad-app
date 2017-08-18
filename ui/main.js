d0eaaa79284c9b69967774b5e50ea170d183e9b4
hannahmargret <30620128+hannahmargret@users.noreply.github.com>
2017-08-18 14:06:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,3 @@
-console.log('Loaded!');
-//change the text of the main-text div
-var element = document.getElementById("main-text");
-element.innerHTML = "new value";
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight()
-{
-   marginLeft = marginLeft+1;
-   img.style.marginLeft = marginLeft + "px";
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-  //img.style.marginLeft = "100px";  
-};
-
 var counter = 0;
 var btn = document.getElementById("counter");
 
