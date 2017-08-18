25e73cc21a36d2e2c3743254626c7a719e626d9b
hannahmargret <30620128+hannahmargret@users.noreply.github.com>
2017-08-18 13:55:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,12 @@ img.onclick = function(){
     var interval = setInterval(moveRight,50);
   //img.style.marginLeft = "100px";  
 };
+
+var counter = 0;
+var btn = document.getElementById("counter");
+
+btn.onclick = function(){
+  counter = counter + 1;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toString();
+};
